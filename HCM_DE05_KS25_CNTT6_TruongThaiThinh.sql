d2348dec1d41888c92b3389625b59d4d657928de
Trương Thái Thịnh <truongthaithinh.htn@gmail.com>
2026-05-04 15:15:44 +0700

    UPDATE

--- HCM_DE05_KS25_CNTT6_TruongThaiThinh.sql
+++ HCM_DE05_KS25_CNTT6_TruongThaiThinh.sql
@@ -4,7 +4,7 @@ USE ESportsManagement;
 CREATE TABLE teams (
 	team_id VARCHAR(10) PRIMARY KEY,
     team_name VARCHAR(100) NOT NULL,
-    country VARCHAR(100),
+    country VARCHAR(50),
     owner VARCHAR(100),
     founded_year INT
 );
@@ -12,9 +12,9 @@ CREATE TABLE teams (
 CREATE TABLE players (
 	player_id VARCHAR(10) PRIMARY KEY,
     full_name VARCHAR(100),
-    nick_name VARCHAR(100),
+    nick_name VARCHAR(50),
     position VARCHAR(50),
-    salary DECIMAL(10, 2),
+    salary DECIMAL(12, 2),
     team_id VARCHAR(10),
     FOREIGN KEY (team_id) REFERENCES teams(team_id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE players (
 CREATE TABLE matchs (
 	match_id VARCHAR(10) PRIMARY KEY,
     start_time DATETIME,
-    result VARCHAR(20)
+    result VARCHAR(50)
 );
 
 CREATE TABLE match_statistics (
@@ -32,35 +32,32 @@ CREATE TABLE match_statistics (
     deaths INT,
     assists INT,
     PRIMARY KEY (match_id, player_id),
-    FOREIGN KEY (match_id) REFERENCES `matchs`(match_id),
+    FOREIGN KEY (match_id) REFERENCES matchs(match_id),
     FOREIGN KEY (player_id) REFERENCES players(player_id)
 );
 
-ALTER TABLE `matchs`
+ALTER TABLE matchs
 ADD prize DECIMAL(15, 2);
 
-ALTER TABLE match_statistics
+ALTER TABLE teams
 CHANGE country region VARCHAR(50);
-
-DROP TABLE match_statistics;
-DROP TABLE `matchs`;
 
 INSERT INTO teams VALUES 
 ('T01','SGP','Vietnam','GAM',2023),
-('T02','Esports','Chiana','skeleton',2022),
+('T02','Esports','China','skeleton',2022),
 ('T03','T1','Korea','SK',2021),
 ('T04','Cloud','USA','EDG group',2022),
-('T05','Edra','ADC',NULL,2024);
+('T05','Edra','Vietnam',NULL,2024);
 
 
 INSERT INTO players VALUES
 ('P01','Nguyen Van A', 'Levi', 'Jungler',100000000,'T01'),
-('P02','Nguyen Van B', 'Caps', 'Mid',200000000,'T03'),
-('P03','Nguyen Van C', 'Faker', 'Mid',150000000,'T02'),
+('P02','Nguyen Van B', 'Caps', 'Mid',200000000,'T02'),
+('P03','Nguyen Van C', 'Faker', 'Mid',150000000,'T03'),
 ('P04','Nguyen Van D', 'Viper', 'ADC',100000000,'T04'),
 ('P05','Nguyen Van E', 'Blaber', 'Jungler',90000000,'T05');
 
-INSERT INTO `matchs` VALUES
+INSERT INTO matchs VALUES
 ('MS_001','2024-01-01 10:00:00','2-1',50000000),
 ('MS_002','2024-01-02 12:00:00','2-0',60000000),
 ('MS_003','2024-01-03 14:00:00','1-2',40000000),
@@ -74,16 +71,24 @@ INSERT INTO match_statistics VALUES
 ('MS_004','P04',12, 4, 5),
 ('MS_005','P05',9, 2, 7);
 
+SET SQL_SAFE_UPDATES = 0;
+
 UPDATE players
 SET salary = salary * 1.2
 WHERE position = 'Jungler';
 
 DELETE FROM teams
-WHERE owner IS NULL;
+WHERE owner = NULL;
+
+SET SQL_SAFE_UPDATES = 1;
 
 SELECT *
-FROM `matchs` 
-WHERE match_is = 'MS_007';
+FROM players
+WHERE salary BETWEEN 50000000 AND 1500000000;
+
+SELECT *
+FROM matchs
+WHERE match_id = 'MS_007';
 
 SELECT nick_name, position 
 FROM players 
@@ -92,3 +97,6 @@ WHERE team_id IN (
     FROM teams
     WHERE region = 'Vietnam'
 );
+
+DROP TABLE match_statistics;
+DROP TABLE matchs;
